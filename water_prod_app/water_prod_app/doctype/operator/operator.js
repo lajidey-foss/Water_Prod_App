e771372eacbf63f8cdda010eea59a91cafaf0da7
lajidey-foss <spryng.managed@gmail.com>
2024-11-10 12:35:03 +0100

    Operator doctype type filters and name format adjustment

--- water_prod_app/water_prod_app/doctype/operator/operator.js
+++ water_prod_app/water_prod_app/doctype/operator/operator.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2024, Jide Olayinka and Other ArdentPros Dev and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Operator", {
-// 	refresh(frm) {
+frappe.ui.form.on("Operator", {
+	refresh(frm) {
+        frm.set_query("tag_link_to", () => {
+            return {
+                filters: [
+                    ["name", "in", ["User", "Employee"]]
+                ]
+            }
+        })
 
-// 	},
-// });
+	},
+    tag_link_to: function () {
+        if(frm.doc.tag_link_to === "User"){
+            frm.doc.user_code = ""
+            frm.doc.user_name = ""
+            cur_frm.refresh_fields(["user_code","user_name"]);
+        } else if (frm.doc.tag_link_to === "Employee"){
+            frm.doc.employee_code = ""
+            frm.doc.employee_name = ""
+            cur_frm.refresh_fields(["employee_code", "employee_name"]);
+        }
+    },
+});
