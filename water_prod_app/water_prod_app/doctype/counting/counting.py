fe3b7febb1f1b8fa424ea14f9108018ccc35a62b
Lajide <lajide@Lajides-MacBook-Pro.local>
2025-08-26 01:06:40 +0100

    Counting doctyp, it details child doctype, working sheet and otheres

---
+++ water_prod_app/water_prod_app/doctype/counting/counting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Jide Olayinka and Other ArdentPros Dev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Counting(Document):
+	pass
