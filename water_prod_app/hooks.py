5d6303c4ce8b6145777bfd04ff0ca518a2bff3f3
Lajide <lajide@Lajides-MacBook-Pro.local>
2025-09-07 20:25:38 +0100

    Production Cycle and Colletion
    Added open count figure to eoc child details column, and link collection to poc also included baggers to collection details table.

--- water_prod_app/hooks.py
+++ water_prod_app/hooks.py
@@ -141,8 +141,8 @@ doc_events = {
 	"Stock Entry": {
 		"after_insert": "water_prod_app.utils.prod_mat.work_flow"
 	},
-    "Purchase Invoice": {"validate": "water_prod_app.utils.prod_mat.purchase_count"},
-    "Collections": {"validate": "water_prod_app.utils.prod_mat.collection_inx"}
+    "Purchase Invoice": { "on_submit": "water_prod_app.utils.prod_mat.purchase_count"},
+    "Collections": { "after_insert": "water_prod_app.utils.prod_mat.collection_inx"}
 }
 
 # Scheduled Tasks
