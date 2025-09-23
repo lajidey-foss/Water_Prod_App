b20f791cd32905c4a633db174953c58296729125
Lajide <lajide@Lajides-MacBook-Pro.local>
2025-09-23 22:51:28 +0100

    Re-Design Loadout Count for Cycle Filter
    RE:Code Prod Loadout Count Quick Entry to accommodate Prod Open Cycle filter status requirement

--- water_prod_app/water_prod_app/doctype/prod_loadout_count/prod_loadout_count.py
+++ water_prod_app/water_prod_app/doctype/prod_loadout_count/prod_loadout_count.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, Jide Olayinka and Other ArdentPros Dev and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
+from frappe.utils import flt, get_datetime
 
 
 class ProdLoadoutCount(Document):
 	pass
+
+@frappe.whitelist()
+def quick_loadout(prd_cyc, load_item, qty, recorder):
+	""" run count"""
+
+	try:
+		doc = frappe.get_doc({
+			#Prod Loadout Count
+			"doctype": "Prod Loadout Count",
+            "prod_cycle": prd_cyc,
+            "item": load_item,
+            "loading_quantity": flt(qty),
+			"counter": recorder
+		}).insert(ignore_permissions=True)
+
+		return doc.name
+	except Exception:
+		frappe.throw(_('Quick Count Failed'), _('Could not create new load out record'))
